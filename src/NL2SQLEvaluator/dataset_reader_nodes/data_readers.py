ba24c886297366a4c61722c8a4601c2d19dba76e
simone papicchio <simopapi@hotmail.com>
2025-10-23 16:51:47 +0200

    feat: add `read_data_from_file` utility and refactor `DataReaderProtocol`
    - Introduced `read_data_from_file` to streamline dataset reading with input validation.
    - Moved `DataReaderProtocol` to a standalone definition for clarity.

--- src/NL2SQLEvaluator/dataset_reader_nodes/data_readers.py
+++ src/NL2SQLEvaluator/dataset_reader_nodes/data_readers.py
@@ -16,13 +16,19 @@ class DataInput(TypedDict):
     input_seq: ChatMessageHF
 
 
+class DataReaderProtocol(Protocol):
+    @staticmethod
+    def read(file_path: str, *args, **kwargs) -> list[DataInput]:
+        ...
+
+
 def enforce_input_type(input_dict: list[dict]):
     for data in input_dict:
         if 'db_file' not in data or 'target_query' not in data or 'input_seq' not in data:
             raise ValueError("Each input data must contain at least 'db_file', 'target_query', and 'input_seq' keys.")
 
 
-class DataReaderProtocol(Protocol):
-    @staticmethod
-    def read(file_path: str, *args, **kwargs) -> list[DataInput]:
-        ...
+def read_data_from_file(reader: DataReaderProtocol, file_path: str, *args, **kwargs) -> list[DataInput]:
+    data = reader.read(file_path, *args, **kwargs)
+    enforce_input_type(data)
+    return data
